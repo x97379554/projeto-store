c49105e10fff51c9a633ef5da8a2ce4fa9ece418
Alex <reigelado@gmail.com>
2024-08-01 16:55:58 -0300

    Melhorando o layout v3

--- script.js
+++ script.js
@@ -95,31 +95,22 @@ function exibirNotebooks(notebooks) {
     row.appendChild(card);
   }
 }
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-white", "");
-  }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-white";
-}
 // Chama a função para buscar os notebooks quando a página carregar
 window.onload = buscarNotebooks;
+const slides = document.querySelectorAll('#slideshow img');
+let currentSlide = 0;
+
+// Define a função para mostrar o próximo slide
+function showNextSlide() {
+  // Esconde o slide atual
+  slides[currentSlide].classList.remove('active');
+
+  // Calcula o índice do próximo slide
+  currentSlide = (currentSlide + 1) % slides.length;
+
+  // Exibe o próximo slide
+  slides[currentSlide].classList.add('active');
+}
+
+// Chama a função para mostrar o próximo slide a cada segundo
+setInterval(showNextSlide, 1000); // 1000 milissegundos = 1 segundo
